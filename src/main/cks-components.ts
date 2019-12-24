8deec4cf1046e715c422d14f038e747d5bd7e6d4
chakannom <genteelson@gmail.com>
2019-12-24 23:01:26 +0900

    added TranslateDirective

--- src/main/cks-components.ts
+++ src/main/cks-components.ts
@@ -4,11 +4,12 @@ import { CksPageRibbonComponent } from './components/profiles/page-ribbon.compon
 import { CksSidebarComponent } from './components/sidebar/sidebar.component';
 import { CksTopbarComponent } from './components/topbar/topbar.component';
 import { CksActiveLanguageDirective } from './directives/active-language.directive';
+import { CksTranslateDirective } from './directives/cks-translate.directive';
 import { CksMainWithHeadComponent } from './layouts/main/with-head/main-with-head.component';
 import { CksMainWithSideAndHeadComponent } from './layouts/main/with-side-and-head/main-with-side-and-head.component';
 
 export const CKS_COMPONENTS = [CksDynamicComponent, CksNavbarComponent, CksPageRibbonComponent, CksSidebarComponent, CksTopbarComponent];
 
-export const CKS_DIRECTIVES = [CksActiveLanguageDirective];
+export const CKS_DIRECTIVES = [CksActiveLanguageDirective, CksTranslateDirective];
 
 export const CKS_LAYOUTS = [CksMainWithHeadComponent, CksMainWithSideAndHeadComponent];
